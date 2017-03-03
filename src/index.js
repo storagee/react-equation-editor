05ad9220a4beb44d8c50c99de2c54d2452381938
zhihuilai <luckyzhihui@gmail.com>
2017-03-03 15:31:07 +0800

    update to umd module

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './css/react-equation-editor.css';
+
+class ReactEquationEditor extends Component {
+    render() {
+        return (
+            <h1>React Equation Editor.</h1>
+        )
+    }
+}
+
+export default ReactEquationEditor;
